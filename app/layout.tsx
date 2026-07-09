67806d35276268e4f188b13654d46ca95ac4c791
Pamela Terrell <pammyhoney@yahoo.com>
2026-07-09 09:24:48 -0400

    Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 
@@ -36,7 +37,22 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+
         <Analytics />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JRJYL8QNWL"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JRJYL8QNWL');
+          `}
+        </Script>
       </body>
     </html>
   );
